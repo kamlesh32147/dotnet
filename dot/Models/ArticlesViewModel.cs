e73dbc0f3371ff5872aa34556638499fa1317cdb
kamlesh32147 <kamleshchoudhary779@yahoo.com>
2021-04-05 09:20:38 +0530

    commit-message

---
+++ dot/Models/ArticlesViewModel.cs
@@ -0,0 +1,24 @@
+
+    using Microsoft.AspNetCore.Mvc;  
+    using System.Collections.Generic;  
+    using System.Net.Http;  
+    using WebApp.Models;  
+      
+    namespace WebApp.Controllers  
+    {  
+        public class HomeController : Controller  
+        {  
+            public IActionResult Index()  
+            {  
+                return View();  
+            }  
+      
+            public IActionResult Articles()  
+            {  
+                var model = new ArticlesViewModel();  
+                model.Articles = new ArticleRepository().GetLatest();  
+      
+                return View(model);  
+            }  
+        }  
+    }  
